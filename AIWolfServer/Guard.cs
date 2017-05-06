e0c465945cd5d6ee8d384482e89c4441a6b61df8
otsuki <otsuki.yamagata.university@gmail.com>
2017-05-07 00:42:51 +0900

    Create AIWolf.Server.Guard class.

---
+++ AIWolfServer/Guard.cs
@@ -0,0 +1,37 @@
+//
+// Guard.cs
+//
+// Copyright (c) 2016 Takashi OTSUKI
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+using AIWolf.Lib;
+
+namespace AIWolf.Server
+{
+    /// <summary>
+    /// Guard class.
+    /// </summary>
+    class Guard
+    {
+        public int Day { get; }
+
+        public Agent Agent { get; }
+
+        public Agent Target { get; }
+
+        public Guard(int day, Agent agent, Agent target)
+        {
+            Day = day;
+            Agent = agent;
+            Target = target;
+        }
+
+        public override string ToString()
+        {
+            return Agent + " guarded " + Target + "@" + Day;
+        }
+    }
+}
