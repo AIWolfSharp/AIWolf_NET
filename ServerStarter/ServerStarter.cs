e06d2116e203b9a34b7be3478ee7cf21d2730837
otsuki <otsuki.yamagata.university@gmail.com>
2017-05-22 18:16:36 +0900

    ServerStarter.cs: Add option -g, -l, -cl.

--- ServerStarter/ServerStarter.cs
+++ ServerStarter/ServerStarter.cs
@@ -9,6 +9,7 @@
 
 using AIWolf.Lib;
 using System;
+using System.IO;
 
 namespace AIWolf.Server
 {
@@ -17,34 +18,57 @@ namespace AIWolf.Server
         public static void Main(string[] args)
         {
             int port = 10000;
-            int agentNum = 12;
+            int agentNum = 5;
+            int gameNum = 1;
+            string logDir = ".";
+            bool showConsoleLog = true;
 
             for (int i = 0; i < args.Length; i++)
             {
                 if (args[i].StartsWith("-"))
                 {
-                    if (args[i].Equals("-p"))
+                    if (args[i] == "-p")
                     {
                         i++;
                         port = int.Parse(args[i]);
                     }
-                    else if (args[i].Equals("-n"))
+                    else if (args[i] == "-n")
                     {
                         i++;
                         agentNum = int.Parse(args[i]);
                     }
+                    else if (args[i] == "-g")
+                    {
+                        i++;
+                        gameNum = int.Parse(args[i]);
+                    }
+                    else if (args[i] == "-l")
+                    {
+                        i++;
+                        logDir = args[i];
+                    }
+                    else if (args[i] == "-cl")
+                    {
+                        showConsoleLog = false;
+                    }
                 }
             }
 
-            Console.WriteLine("Start AIWolf Server port:{0} playerNum:{1}", port, agentNum);
+            Console.WriteLine("Start AIWolf.NET Server port:{0} playerNum:{1} gameNum:{2}", port, agentNum, gameNum);
             GameSetting gameSetting = GameSetting.GetDefaultGameSetting(agentNum);
 
             TcpipServer server = new TcpipServer(port, agentNum, gameSetting);
             server.WaitForConnection();
-            AIWolfGame game = new AIWolfGame(gameSetting, server);
-            game.GameLogger = new FileGameLogger(".");
-            game.Rand = new Random();
-            game.Start();
+            string logSubDir = Path.Combine(Path.GetFullPath(logDir), DateTime.Now.ToString("yyyyMMddHHmmss"));
+            for (int i = 0; i < gameNum; i++)
+            {
+                string logName = Path.Combine(logSubDir, i.ToString("D3") + ".log");
+                AIWolfGame game = new AIWolfGame(gameSetting, server);
+                game.ShowConsoleLog = showConsoleLog;
+                game.GameLogger = new FileGameLogger(logName);
+                game.Rand = new Random();
+                game.Start();
+            }
         }
     }
 }
