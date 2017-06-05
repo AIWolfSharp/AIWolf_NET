e9abaa05c535cfdbe67b638f226d1720a0af1e68
otsuki <otsuki.yamagata.university@gmail.com>
2017-06-05 12:51:51 +0900

    Make Utterance not serializable.

--- AIWolfLib/Utterance.cs
+++ AIWolfLib/Utterance.cs
@@ -8,7 +8,6 @@
 //
 
 using Newtonsoft.Json;
-using System.Runtime.Serialization;
 
 namespace AIWolf.Lib
 {
@@ -21,7 +20,6 @@ namespace AIWolf.Lib
     /// Abstract utterance class.
     /// </summary>
 #endif
-    [DataContract]
     public abstract class Utterance
     {
 #if JHELP
@@ -33,7 +31,7 @@ namespace AIWolf.Lib
         /// There is nothing to utter.
         /// </summary>
 #endif
-        public const string OVER = "Over";
+        public static readonly string OVER = "Over";
 
 #if JHELP
         /// <summary>
@@ -44,13 +42,7 @@ namespace AIWolf.Lib
         /// Skip this turn though there is something to utter.
         /// </summary>
 #endif
-        public const string SKIP = "Skip";
-
-        /// <summary>
-        /// The index number of the agent who uttered.
-        /// </summary>
-        [DataMember(Name = "agent")]
-        int _agent;
+        public static readonly string SKIP = "Skip";
 
 #if JHELP
         /// <summary>
@@ -61,7 +53,6 @@ namespace AIWolf.Lib
         /// The index number of this utterance.
         /// </summary>
 #endif
-        [DataMember(Name = "idx")]
         public int Idx { get; }
 
 #if JHELP
@@ -73,7 +64,6 @@ namespace AIWolf.Lib
         /// The day of this utterance.
         /// </summary>
 #endif
-        [DataMember(Name = "day")]
         public int Day { get; }
 
 #if JHELP
@@ -85,7 +75,6 @@ namespace AIWolf.Lib
         /// The turn of this utterance(optional). -1 if not specified.
         /// </summary>
 #endif
-        [DataMember(Name = "turn")]
         public int Turn { get; } = -1;
 
 #if JHELP
@@ -97,7 +86,7 @@ namespace AIWolf.Lib
         /// The agent who uttered.
         /// </summary>
 #endif
-        public Agent Agent => Agent.GetAgent(_agent);
+        public Agent Agent { get; }
 
 #if JHELP
         /// <summary>
@@ -108,40 +97,7 @@ namespace AIWolf.Lib
         /// The contents of this utterance.
         /// </summary>
 #endif
-        [DataMember(Name = "text")]
         public string Text { get; }
-
-#if JHELP
-        /// <summary>
-        /// 発話の新しいインスタンスを初期化する
-        /// </summary>
-        /// <param name="idx">発話のインデックス</param>
-        /// <param name="day">発話日</param>
-#else
-        /// <summary>
-        /// Initializes a new instance of this class.
-        /// </summary>
-        /// <param name="idx">The index of this utterance.</param>
-        /// <param name="day">The day of this utterance.</param>
-#endif
-        protected Utterance(int idx, int day)
-        {
-            Idx = idx;
-            if (Idx < 0)
-            {
-                Error.RuntimeError("Invalid idx " + Idx + ".");
-                Idx = 0;
-                Error.Warning("Force it to be " + Idx + ".");
-            }
-
-            Day = day;
-            if (Day < 0)
-            {
-                Error.RuntimeError("Invalid day " + Day + ".");
-                Day = 0;
-                Error.Warning("Force it to be " + Day + ".");
-            }
-        }
 
 #if JHELP
         /// <summary>
@@ -158,8 +114,10 @@ namespace AIWolf.Lib
         /// <param name="day">The day of this utterance.</param>
         /// <param name="turn">The turn of this utterance.</param>
 #endif
-        protected Utterance(int idx, int day, int turn) : this(idx, day)
+        protected Utterance(int idx = 0, int day = 0, int turn = -1)
         {
+            Idx = idx;
+            Day = day;
             Turn = turn;
         }
 
@@ -182,19 +140,10 @@ namespace AIWolf.Lib
         /// <param name="agent">The agent who uttered.</param>
         /// <param name="text">The text of this utterance.</param>
 #endif
-        protected Utterance(int idx, int day, int turn, Agent agent, string text) : this(idx, day, turn)
+        protected Utterance(int idx, int day, int turn, Agent agent, string text)
+            : this(idx, day, turn)
         {
-            if (agent == null)
-            {
-                Error.RuntimeError("Agent must not be null.");
-                _agent = 0;
-                Error.Warning("Force it to be " + Agent + ".");
-            }
-            else
-            {
-                _agent = agent.AgentIdx;
-            }
-
+            Agent = agent;
             Text = text;
         }
 
@@ -218,9 +167,8 @@ namespace AIWolf.Lib
         /// <param name="text">The text of this utterance.</param>
 #endif
         [JsonConstructor]
-        protected Utterance(int idx, int day, int turn, int agent, string text) : this(idx, day, turn, Agent.GetAgent(agent), text)
-        {
-        }
+        protected Utterance(int idx, int day, int turn, int agent, string text)
+            : this(idx, day, turn, Agent.GetAgent(agent), text) { }
 
 #if JHELP
         /// <summary>
@@ -252,9 +200,7 @@ namespace AIWolf.Lib
         /// </summary>
         /// <param name="idx">The index of this talk.</param>
         /// <param name="day">The day of this talk.</param>
-        internal Talk(int idx, int day) : base(idx, day)
-        {
-        }
+        internal Talk(int idx, int day) : base(idx, day) { }
 
 #if JHELP
         /// <summary>
@@ -275,9 +221,8 @@ namespace AIWolf.Lib
         /// <param name="agent">The agent who talked.</param>
         /// <param name="text">The text of this talk.</param>
 #endif
-        public Talk(int idx, int day, int turn, Agent agent, string text) : base(idx, day, turn, agent, text)
-        {
-        }
+        public Talk(int idx, int day, int turn, Agent agent, string text)
+            : base(idx, day, turn, agent, text) { }
 
         /// <summary>
         /// Initializes a new instance of this class.
@@ -288,9 +233,8 @@ namespace AIWolf.Lib
         /// <param name="agent">The index of agent who talked.</param>
         /// <param name="text">The text of this talk.</param>
         [JsonConstructor]
-        Talk(int idx, int day, int turn, int agent, string text) : base(idx, day, turn, agent, text)
-        {
-        }
+        Talk(int idx, int day, int turn, int agent, string text)
+            : base(idx, day, turn, agent, text) { }
 
 #if JHELP
         /// <summary>
@@ -303,10 +247,7 @@ namespace AIWolf.Lib
         /// </summary>
         /// <returns>A string that represents the current object.</returns>
 #endif
-        public override string ToString()
-        {
-            return string.Format("Talk: Day{0:D2} {1:D2}[{2:D3}]\t{3}\t{4}", Day, Turn, Idx, Agent, Text);
-        }
+        public override string ToString() => $"Talk: Day{Day:D2} {Turn:D2}[{Idx:D3}]\t{Agent}\t{Text}";
     }
 
 #if JHELP
@@ -325,9 +266,7 @@ namespace AIWolf.Lib
         /// </summary>
         /// <param name="idx">The index of this whisper.</param>
         /// <param name="day">The day of this whisper.</param>
-        internal Whisper(int idx, int day) : base(idx, day)
-        {
-        }
+        internal Whisper(int idx, int day) : base(idx, day) { }
 
 #if JHELP
         /// <summary>
@@ -348,9 +287,8 @@ namespace AIWolf.Lib
         /// <param name="agent">The agent who talked.</param>
         /// <param name="text">The text of this talk.</param>
 #endif
-        public Whisper(int idx, int day, int turn, Agent agent, string text) : base(idx, day, turn, agent, text)
-        {
-        }
+        public Whisper(int idx, int day, int turn, Agent agent, string text)
+            : base(idx, day, turn, agent, text) { }
 
         /// <summary>
         /// Initializes a new instance of this class.
@@ -361,9 +299,8 @@ namespace AIWolf.Lib
         /// <param name="agent">The index of agent who whispered.</param>
         /// <param name="text">The text of this whisper.</param>
         [JsonConstructor]
-        Whisper(int idx, int day, int turn, int agent, string text) : base(idx, day, turn, agent, text)
-        {
-        }
+        Whisper(int idx, int day, int turn, int agent, string text)
+            : base(idx, day, turn, agent, text) { }
 
 #if JHELP
         /// <summary>
@@ -376,10 +313,6 @@ namespace AIWolf.Lib
         /// </summary>
         /// <returns>A string that represents the current object.</returns>
 #endif
-        public override string ToString()
-        {
-            return string.Format("Whisper: Day{0:D2} {1:D2}[{2:D3}]\t{3}\t{4}", Day, Turn, Idx, Agent, Text);
-        }
+        public override string ToString() => $"Whisper: Day{Day:D2} {Turn:D2}[{Idx:D3}]\t{Agent}\t{Text}";
     }
-
 }
